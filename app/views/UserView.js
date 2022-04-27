45fddf5d914a000fdee42cb56e606eac9e6eebb8
Jorge Luis Marquez Leon <99061178+JorgeLMarquez@users.noreply.github.com>
2022-04-27 14:36:58 -0500

    Add files via upload

---
+++ app/views/UserView.js
@@ -0,0 +1,21 @@
+const UserService = require('./../../app/services/UserService')
+class UserView {
+
+    static createUser(payload) {
+
+        if (payload === null) {
+            return {
+                error: "payload no existe"
+            }
+        } else if ((typeof payload.username !== 'string' || payload.username === null) || (typeof payload.name !== 'string' || payload.name === null) || (typeof payload.id !== 'number' || payload.id === null)) {
+            return {
+                error: "necesitan tener un valor válido"
+            }
+        } else {
+
+            return UserService.create(payload.id, payload.username, payload.name)
+        }
+    }
+}
+
+module.exports = UserView;
